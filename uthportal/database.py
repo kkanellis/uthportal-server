b0dc0f25cf63b9ed711451ea81d50cad4b3e6378
Konstantinos Kanellis <kkanelli@hotmail.com>
2014-11-16 18:37:12 +0200

    database.py: Exceptions are raising properly at connect method

--- uthportal/database.py
+++ uthportal/database.py
@@ -2,6 +2,7 @@ import logging
 from abc import ABCMeta, abstractmethod
 
 from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
 
 logger = logging.getLogger(__name__)
 
@@ -52,6 +53,8 @@ class MongoDatabaseManager(IDatabaseManager):
         super(MongoDatabaseManager, self).__init__(self, **kargs)
 
     def connect(self, *args, **kargs):
+        self.client = None
+
         try:
             self.client = MongoClient(
                     host=self.info['host'],
@@ -60,23 +63,24 @@ class MongoDatabaseManager(IDatabaseManager):
         except ConnectionFailure:
             logger.error('Cannot connect to database (%s, %s)' % (self.info['host'], self.info['port']))
         except KeyError:
-            logger.error('Key not found in info! Host=%s, Port=%s' % (self.info['host'], self.info['port']))
+            logger.error('"host" or "port" not found in info dictionary!')
         except TypeError:
-            logger.error('Maybe port is NOT (int)? type(port)=%s' % str(type(port)) )
+            logger.error('Maybe port is NOT defined or not (int)?' )
         except Exception as ex:
             logger.error(ex.message)
 
         # Perform self-check
-        if not self.client.alive():
+        if (not self.client) or (not self.client.alive()):
             logger.error('MongoDB client is NOT alive! (%s, %s)' % (self.info['host'], self.info['port']))
-        else:
-            logger.debug('Connected to MongoDB successfully!')
+            return False
 
-        return self.client.alive()
+        logger.debug('Connected to MongoDB successfully!')
+        return True
 
     def disconnect(self):
         """ No need to close connections. This is handled by pymongo! """
-        self.client.disconnect()
+        if self.client:
+            self.client.disconnect()
 
     def insert_document(self, collection, document, **kargs):
         pass
