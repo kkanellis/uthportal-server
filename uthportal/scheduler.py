4ff4aa5318a2c706cb04d23c1a152eb2ab7bf792
Konstantinos Kanellis <kkanelli@hotmail.com>
2015-02-09 16:35:54 +0200

    scheduler.py: Implemented basic functionality

--- uthportal/scheduler.py
+++ uthportal/scheduler.py
@@ -16,24 +16,123 @@ passed as arguments:
 
 """
 
+import logging
+
+import gevent
+from apscheduler.schedulers.base import BaseScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.jobstores.base import JobLookupError, ConflictingIdError
+
 import tasks
 import library.inf
 import data.inf
 import data.uth
 
+logger = logging.getLogger(__name__)
+
 class Scheduler(object):
     def __init__(self, tasks, apscheduler_kwargs, interval_dict):
-        pass
 
-    def add_task(name, func, interval=None):
-        pass
+        if not isinstance(tasks, dict):
+            logger.error('tasks arg not a dictionary')
+            return
 
-    def remove_task(name):
-        pass
+        self.tasks = tasks
+        self.intervals = interval_dict
 
-    def init_queue(apscheduler_kwargs, interval_dict):
-        pass
+        logger.debug('Check tasks paths!')
+        # TODO: Check if paths are valid
+        self.init(apscheduler_kwargs)
 
-    def clear_queue():
-        pass
+    def init(self, apscheduler_kwargs):
+        """ Initializes the queue, and adds the tasks """
+
+        self.sched = BackgroundScheduler(apscheduler_kwargs, logger=logger)
+
+        for dept in self.tasks:
+            for task in self.tasks[dept]:
+                _class = self.tasks[dept][task]
+                _class_str = str(type(_class))
+
+                if not _class_str in self.intervals:
+                    logger.warning('Interval not defined for "%s" class' % _class_str)
+                    continue
+
+                id = '%s.%s' % (dept, task)
+                self.add_task(id, _class, self.intervals[_class_str])
+
+        self.start()
+
+    def clear(self):
+        """ Removes all jobs from scheduler """
+        if not isinstance(self.sched, BaseScheduler):
+            logger.error('Scheduler is not initialized')
+            return
+
+        for job in self.sched.get_jobs():
+            job.remove()
+
+    def start(self):
+        """ Start the scheduler by starting the instance of APScheduler """
+        if not isinstance(self.sched, BaseScheduler):
+            logger.error('Scheduler is not initialized')
+            return
+
+        try:
+            self.sched.start()
+        except SchedulerAlreadyRunningError, e:
+            logger.warning(e)
+
+
+    def stop(self, wait=True):
+        """ Stop the scheduler. If wait=True, then it will be stopped after
+            all jobs that are currently executed will finish """
+        if not isinstance(self.sched, BaseScheduler):
+            logger.warning('Scheduler is not initialized')
+            return
+
+        try:
+            self.sched.shutdown(wait=wait)
+        except SchedulerNotRunningError, e:
+            logger.warning(e)
+
+    def add_task(self, id, func, interval=None):
+        """ Adds a new task into the queue. If interval is None then the task
+            will be executed once. """
+        if not isinstance(id, basestring):
+            logger.error('"id" argument is not an instance of basestring')
+            return
+
+        if not hasattr(func, '__call__'):
+            logger.error('"func" is not callable')
+            return
+
+        try:
+            if isinstance(interval, datetime):
+                self.sched.add_job(func, 'interval', id, interval)
+            elif isinstace is None: # Run once (ommit trigger)
+                self.sched.add_job(func, id=id)
+            else:
+                logger.error('"interval" is not an instance of [datetime|None]')
+                return
+        except ConfilictingIdError, e:
+            logger.warning(e)
+
+    def remove_task(self, id):
+        """ Remove a job from the queue """
+        if not isinstance(id, basestring):
+            logger.error('"id" argument is not an instance of basestring')
+            return
+
+        try:
+            self.sched.remove_job(id)
+        except JobLookupError, e:
+            logger.warning(e)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+
+    sched = Scheduler({ }, { }, { })
+    sched.stop()
 
