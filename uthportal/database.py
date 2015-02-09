171578b297768a9e6cdf20f3e7bea2cef2c47ecf
George T. Gougoudis <george_gougoudis@hotmail.com>
2015-02-09 07:23:30 +0200

    added interface module for interfaces

--- uthportal/database.py
+++ uthportal/database.py
@@ -1,53 +1,11 @@
 
 import logging
-from abc import ABCMeta, abstractmethod
+from interface.database import IDatabaseManager
 
 from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure, OperationFailure
 
 logger = logging.getLogger(__name__)
-
-class IDatabaseManager(object):
-    __metaclass__ = ABCMeta
-
-    def __init__ (self, **kwargs):
-        """ info: dictionary of arguments
-
-        Neccessary keys are: host, port & db_name .
-        TODO: Maybe use named arguments or get with default values?
-        """
-        self.info = kwargs
-
-    @abstractmethod
-    def connect(self, *args, **kwargs):
-        """ Connect to database. Returns boolean if it was successfull. """
-        return
-
-    @abstractmethod
-    def disconnect(self):
-        """ Disconnect from databse, closing all active connections/queries. """
-        return
-
-    @abstractmethod
-    def insert_document(self, collection, document, **kwargs):
-        """ Inserts an entry/document to a table/collection. """
-        return
-
-    @abstractmethod
-    def remove_document(self, collection, query, **kwargs):
-        """ Deletes an entry/document from a table/collection. """
-        return
-
-    @abstractmethod
-    def find_document(self, collection, query, **kwargs):
-        """ Finds and returns the entry/document from a table/collection. """
-        return
-
-    @abstractmethod
-    def update_document(self, collection, query, document, **kwargs):
-        """ Updates an existing document with new data """
-        return
-
 
 class MongoDatabaseManager(IDatabaseManager):
 
