8abe21b05bf6c035594a193c4d6ae06ba490171d
George T. Gougoudis <george.gougoudis@gmail.com>
2015-02-09 15:03:26 +0200

    added base task

---
+++ uthportal/interface/tasks.py
@@ -0,0 +1,36 @@
+from abc import ABCMeta, abstractmethod
+
+class BaseTask(object):
+    __metaclass__ = ABCMeta
+
+    def __init__ (self, database_manager, **kwargs):
+        self.args = kwargs
+        self.database_manager = database_manager
+
+    @abstractmethod
+    def fetch(self, *args, **kwargs):
+        """Fetch a remote document to be parsed later"""
+        return
+
+    def save(self, *args, **kwargs):
+        #TODO: implement
+        """Save result dictionary in database"""
+        return
+
+    def load(self, *args, **kwargs):
+        #TODO: implement
+        """Load old dictionary from database"""
+        return
+
+    @abstractmethod
+    def __call__(self, *args, **kwargs):
+        """This is the method called from the Scheduler when this object is
+        next in queue"""
+        return
+
+
+class CourseTask(BaseTask):
+
+    def __init__():
+        pass
+
