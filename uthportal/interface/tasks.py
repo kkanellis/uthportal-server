e514e83e6a0cb39dd1f374aab2061bed7cbb83d6
Konstantinos Kanellis <kkanelli@hotmail.com>
2015-02-10 15:28:13 +0200

    tasks.py: Rewriten update & impoved inheritances

--- uthportal/interface/tasks.py
+++ uthportal/interface/tasks.py
@@ -22,6 +22,12 @@ class BaseTask(object):
         self.id = path.split('.')[-1]
         self.db_collection = path.split('.')[:-1]
 
+    def __call__(self):
+        """This is the method called from the Scheduler when this object is
+        next in queue"""
+
+        self.update()
+
     def fetch(self, link):
         """Fetch a remote document to be parsed later"""
         try:
@@ -42,93 +48,105 @@ class BaseTask(object):
 
     @abstractmethod
     def update(self, *args, **kwargs):
-        """This function is called from __call__"""
+        """This function is called from __call__. Takes as a key-word argument (kwargs) a dictionary called
+            new_fields where new data are stored after fecthing procedures. These are compared with the
+            current data (stored in self.document)"""
 
-        old = kwargs['old']
-        new = kwargs['new']
-        if not old or not new:
+        """ Check if 'new_fields' arg is present """
+        if 'new_fields' in kwargs:
+            new_fields = kwargs['new_fields']
+        else:
+            logger.warning('[%s] Update method called without "new_fields" dict' % self.path)
             return
 
+        """ Check if 'new_fields' has the neccessary fields """
+        for field in self.update_fields:
+            if field not in new_fields:
+                logger.warning('[%s] Field "%s" not present in "new_fields" dict' % (self.path, field))
+                return
+
+        """ Get self.document's update_fields """
+        old_fields = { field: self.__get_document_field(self.document, field)
+                            for field in self.update_fields }
+
+        """ Checking for differences in the according update_fields """
         differ = False
-        for key in new: # This will be changed. Too swag code
-            if new[key] and key in old and old[key] and new[key] != old[key]:
+        for field in self.update_fields:
+            if old_fields[field] != new_fields[field]:
+                logger.info('[%s] New entries in field "%s"' % (self.path, new_fields[field]))
                 differ = True
                 break
 
+        now = datetime.now()
         if differ:
             self.archive()
-            self.update_document(new)
-            self.update_server()
+
+            """ Update new fields """
+            for field in self.update_fields:
+                self.__set_document_field(self.document, field, new_fields[field])
+
+            """ Update remaining fields """
+            self.__set_document_field(self.document, 'first_updated', now)
+            self.__set_document_field(self.document, 'last_updated', now)
+
+            self.transmit()
             self.notify()
         else:
-            self.db_document['last_updated'] = datetime.now()
+            self.__set_document_field(self.document, 'last_updated', now)
 
         self.save()
-
-    def update_document(self, entries):
-        new_document = self.db_document
-
-        keys_exist = True
-        for field in self.fields:
-            if not field in entries:
-                keys_exist = False
-                break
-
-            self.__set_document_field(self, new_document, field, entry[field])
-
-        if keys_exist:
-            new_document['first_updated'] = new_document['last_updated'] = datetime.now()
-            self.db_document = new_document
-        else:
-            pass
-
 
     def notify(self):
         pass
 
+    """ Database related method """
+
     def save(self, *args, **kwargs):
         """Save result dictionary in database"""
-        if not self.database_manager.update_document(self.db_collection, self.db_query, self.db_document, *args, **kwargs):
+        if not self.database_manager.update_document(self.db_collection, self.db_query, self.document, *args, **kwargs):
             logger.warning('Could not save document "%s"' % self.path)
 
     def archive(self, *args, **kwargs):
         """ Save the current document into the history collection """
-        if not self.database_manager.insert_document('history.%s' % self.db_collection, self.db_document, *args, **kwargs):
+        if not self.database_manager.insert_document('history.%s' % self.db_collection, self.document, *args, **kwargs):
             logger.warning('Could not archive document "%s"' % self.path)
+
+    def transmit(self, *args, **kwargs):
+        """ Save the current document into the server collection free of uneccessary fields """
+        pass
 
     def load(self, *args, **kwargs):
         """Load old dictionary from database"""
         return self.database_manager.find_document(self.db_collection, self.db_query, *args, **kwargs)
 
-    def __call__(self):
-        """This is the method called from the Scheduler when this object is
-        next in queue"""
+    """ Helper methods """
 
-        self.update()
+    def __set_document_field(self, document, field, value):
+        pass
+
+    def __get_document_field(self, document, field):
+        pass
+
 
 
 class CourseTask(BaseTask):
     def __init__(self, path, timeout, database_manager):
         super(CourseTask, self).__init__(path, timeout, database_manager)
 
-        self.fields = { 'announcements.site', 'announcements.eclass' }
+        self.update_fields =[ 'announcements.site', 'announcements.eclass' ]
         self.db_query = { 'code' : self.id }
-        self.db_document = self.load()
+        self.document = self.load()
 
     def update(self):
-        old = {
-                'site': self.db_document['announcements']['site'],
-                'eclass': self.db_document['announcements']['eclass']
-        }
-        new = {
-                'site': self.__check_site(),
-                'eclass': self.__check_eclass()
+        new_document_fields = {
+                'announcements.site': self.__check_site(),
+                'announcements.eclass': self.__check_eclass()
         }
 
-        super(CourseTask, self).update(old=old, new=new)
+        super(CourseTask, self).update(new_fields=new_document_fields)
 
     def __check_site(self):
-        link = self.db_document['announcements']['link_site']
+        link = self.document['announcements']['link_site']
         if not link:
             return None
 
@@ -142,7 +160,6 @@ class CourseTask(BaseTask):
 
         return self.parse_site(bsoup)
 
-    @abstractmethod
     def parse_site(self, bsoup):
         """Parse the fetced document"""
         return
@@ -216,7 +233,8 @@ class FoodmenuTask(BaseTask):
         return
 
 
+"""
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     a = CourseTask('inf.courses.ce120', 5, MongoDatabaseManager())
-
+"""
