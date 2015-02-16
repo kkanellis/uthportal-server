222cd80cd07857bbd6349adef7a19aeeaf9103fc
Konstantinos Kanellis <kkanelli@hotmail.com>
2015-02-16 22:08:05 +0200

    tasks/base.py: Included logger in class

--- uthportal/tasks/base.py
+++ uthportal/tasks/base.py
@@ -7,13 +7,14 @@ import requests
 from requests.exceptions import ConnectionError, Timeout
 
 from uthportal.database.mongo import MongoDatabaseManager
-
-logger = logging.getLogger(__name__)
+from uthportal.logger import get_logger, logging_level
 
 class BaseTask(object):
     __metaclass__ = ABCMeta
 
     def __init__(self, path, timeout, database_manager, **kwargs):
+        self.logger = get_logger('%s.%s' % (__name__, self.__class__.__name__), logging_level.DEBUG)
+
         self.path = path
         self.timeout = timeout
         self.database_manager = database_manager
@@ -24,7 +25,7 @@ class BaseTask(object):
     def __call__(self):
         """This is the method called from the Scheduler when this object is
         next in queue"""
-
+        print self.document
         self.update()
 
     def fetch(self, link):
@@ -32,14 +33,14 @@ class BaseTask(object):
         try:
             page = requests.get(link, timeout=self.timeout)
         except ConnectionError:
-            logger.warning('[%s] %s: Connection error' % (self.id, link))
+            self.logger.warning('%s: Connection error' % link)
             return None
         except Timeout:
-            logger.warning('[%s] %s: Timeout [%d]' % (self.id, link, self.timeout))
+            self.logger.warning('%s: Timeout [%d]' % (link, self.timeout))
             return None
 
         if page.code is not (200 or 301):
-            logger.warning('[%s] %s: Returned [%d]' % (self.id, link, page.code))
+            self.logger.warning('%s: Returned [%d]' % (link, page.code))
             return None
 
         page.encoding = 'utf-8'
@@ -55,13 +56,13 @@ class BaseTask(object):
         if 'new_fields' in kwargs:
             new_fields = kwargs['new_fields']
         else:
-            logger.warning('[%s] Update method called without "new_fields" dict' % self.path)
+            self.logger.warning('Update method called without "new_fields" dict')
             return
 
         #Check if 'new_fields' has the neccessary fields
         for field in self.update_fields:
             if field not in new_fields:
-                logger.warning('[%s] Field "%s" not present in "new_fields" dict' % (self.path, field))
+                self.logger.warning('Field "%s" not present in "new_fields" dict' % field)
                 return
 
         #Get self.document's update_fields
@@ -72,7 +73,7 @@ class BaseTask(object):
         differ = False
         for field in self.update_fields:
             if old_fields[field] != new_fields[field]:
-                logger.info('[%s] New entries in field "%s"' % (self.path, new_fields[field]))
+                self.logger.info('New entries in field "%s"' % new_fields[field])
                 differ = True
                 break
 
@@ -103,19 +104,19 @@ class BaseTask(object):
     def save(self, *args, **kwargs):
         """Save result dictionary in database"""
         if not self.database_manager.update_document(self.db_collection, self.db_query, self.document, *args, **kwargs):
-            logger.warning('Could not save document "%s"' % self.path)
+            self.logger.warning('Could not save document "%s"' % self.path)
 
     def archive(self, *args, **kwargs):
         """ Save the current document into the history collection """
         if not self.database_manager.insert_document('history.%s' % self.db_collection, self.document, *args, **kwargs):
-            logger.warning('Could not archive document "%s"' % self.path)
+            self.logger.warning('Could not archive document "%s"' % self.path)
 
     def transmit(self, *args, **kwargs):
         """ Save the current document into the server collection free of uneccessary fields """
         #TODO: Implement ignore_fields
 
         if not self.database_manager.update_document('server.%s' % self.db_collection, self.document, *args, **kwargs):
-            logger.warning('Could not transmit document "%s"' %self.path)
+            self.logger.warning('Could not transmit document "%s"' %self.path)
         pass
 
     def load(self, *args, **kwargs):
@@ -129,7 +130,7 @@ class BaseTask(object):
         keys = field.split('.')
         for key in keys[:-1]:
             if key not in document:
-                logger.warning('[%s] Key "%s" not found in field "%s"' % (self.id, key, field))
+                self.logger.warning('Key "%s" not found in field "%s"' % (key, field))
                 return
 
             document = document[key]
@@ -142,7 +143,7 @@ class BaseTask(object):
         keys = fields.split('.')
         for key in keys[:-1]:
             if key not in document:
-                logger.warning('[%s] Key "%s" not found in field "%s"' % (self.id, key, field))
+                self.logger.warning('Key "%s" not found in field "%s"' % (key, field))
                 return
 
             document = document[key]
