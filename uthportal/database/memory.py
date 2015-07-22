c9272bf31068c59ea9ea8383e05a3e94d12dc797
Konstantinos Kanellis <kkanelli@hotmail.com>
2015-07-22 19:16:03 +0300

    Minor additions to memory.py (database)
    - Add upsert support in remove & update methods

--- uthportal/database/memory.py
+++ uthportal/database/memory.py
@@ -14,22 +14,20 @@ class MemoryDatabaseManager(IDatabaseManager):
     def disconnect(self):
         return True
 
-    def insert_document(self, collection, document):
+    def insert_document(self, collection, document, **kwargs):
         if collection not in self.db:
             self.db[collection] = [ ]
 
         self.db[collection].append(document)
         return True
 
-    def remove_document(self, collection, query, multi=False):
+    def remove_document(self, collection, query, multi=False, **kwargs):
         if collection not in self.db:
-            return None
+            return False
 
-        found = False
         for (index, document) in enumerate(self.db[collection]):
             if all( item in document and query[item] == document[item]
                     for item in query ):
-                found = True
                 del self.db[collection][index]
 
                 if not multi:
@@ -37,7 +35,7 @@ class MemoryDatabaseManager(IDatabaseManager):
 
         return True
 
-    def find_document(self, collection, query):
+    def find_document(self, collection, query, **kwargs):
         if collection not in self.db:
             return None
 
@@ -48,7 +46,7 @@ class MemoryDatabaseManager(IDatabaseManager):
 
         return None
 
-    def find_documents(self, collection, query):
+    def find_documents(self, collection, query, **kwargs):
         if collection not in self.db:
             return None
 
@@ -60,17 +58,26 @@ class MemoryDatabaseManager(IDatabaseManager):
 
         return documents
 
-    def update_document(self, collection, query, new_document, multi=False):
+    def update_document(self, collection, query, new_document, multi=False, upsert=False, **kwargs):
         if collection not in self.db:
+            # If upsert=True then we insert the document
+            if upsert:
+                return self.insert_document(collection, new_document)
+
             return None
 
+        found = False
         for (index, document) in enumerate(self.db[collection]):
             if all( item in document and query[item] == document[item]
                     for item in query ):
                 self.db[collection][index] = new_document
 
+                found = True
                 if not multi:
                     break
+
+        if upsert and not found:
+            return self.insert_document(collection, new_document)
 
         return True
 
