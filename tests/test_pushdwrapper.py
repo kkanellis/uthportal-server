e8b306baec990d046f9b00ac10c5caf12b337cd0
GeorgeTG <george_gougoudis@hotmail.com>
2015-09-25 22:55:28 +0300

    fixed test

--- tests/test_pushdwrapper.py
+++ tests/test_pushdwrapper.py
@@ -3,12 +3,12 @@ from __future__ import absolute_import
 import unittest
 import os
 
-from uthportal.notifier import PushdWrapper
+from uthportal.notifier import Pushd
 from uthportal.configure import Configuration
 from uthportal.logger import get_logger
 from uthportal.database.memory import MemoryDatabaseManager
 
-class TestPushWrapper(unittest.TestCase):
+class TestPushd(unittest.TestCase):
     def setUp(self):
         self.settings = Configuration().get_settings()
         self.settings['log_dir'] = os.path.abspath("test_logs")
@@ -20,19 +20,19 @@ class TestPushWrapper(unittest.TestCase):
                                     'msg' : 'this is a message, for real'
                                     }
                     }
-        self.pushd_wrapper = PushdWrapper(self.settings, self.db_manager, templates)
+        self.pushd = Pushd(self.settings, self.db_manager, templates)
         self.logger = get_logger('test', self.settings)
 
     def routine(self):
-        self.assertTrue(self.pushd_wrapper.is_alive())
-        user_id = self.pushd_wrapper.users.register('gcm', 'testtoken@!#123')
+        self.assertTrue(self.pushd.is_alive())
+        user_id = self.pushd.users.register('gcm', 'testtoken@!#123')
         self.assertIsNotNone(user_id)
         #Add user to databse
         self.db_manager.insert_document("users.active",
         {'email' : 'test@uth.gr', 'token': '12345678', 'pushd_id': user_id})
-        self.assertTrue('test@uth.gr' in self.pushd_wrapper.users)
+        self.assertTrue('test@uth.gr' in self.pushd.users)
         #try to get user with getter
-        pushd_user = self.pushd_wrapper.users['test@uth.gr']
+        pushd_user = self.pushd.users['test@uth.gr']
         self.assertIsNotNone(pushd_user)
         user_info = pushd_user.info()
         self.assertIsNotNone(user_info)
@@ -48,6 +48,6 @@ class TestPushWrapper(unittest.TestCase):
 
 
         #unregister user
-        self.assertTrue(self.pushd_wrapper.users.unregister('test@uth.gr'))
+        self.assertTrue(self.pushd.users.unregister('test@uth.gr'))
     def test_pusd_wrapper(self):
         self.routine()
