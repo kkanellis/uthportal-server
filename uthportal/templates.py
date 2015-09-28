ee77f778a223071edbfa9422f1b577ad237ce4e8
Konstantinos Kanellis <kkanelli@hotmail.com>
2015-09-28 17:56:04 +0300

    [templates] Add initial pushd templates

---
+++ uthportal/templates.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+event_templates = {
+        'inf.announce': {
+            'title.gr': '${var.name}',
+            'msg.gr': 'Νέα ανακοίνωση!'
+        },
+        'inf.courses': {
+            'title.gr': '${var.name}',
+            'msg.gr': '${var.code_site}: Νέα ανακοίνωση!'
+        },
+        'uth.announce': {
+            'title.gr': '${var.name}',
+            'msg.gr': 'Νέα ανακοίνωση!'
+        },
+        'uth.food': {
+            'title.gr': 'Μενού Λέσχης',
+            'msg.gr': 'Ενημέρωθηκε ${var.date}'
+        }
+}
+
+
